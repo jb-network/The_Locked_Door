7f14908d9e06c42180a95253fd355b0708425976
jb-network <85036487+jb-network@users.noreply.github.com>
2022-06-11 11:08:32 -0500

    Created Menu

--- The_Locked_Door/Program.cs
+++ The_Locked_Door/Program.cs
@@ -4,22 +4,41 @@
 // To unlock a code must be used
 // The user must be able to change the code, if the current code is provided. 
 
-Console.WriteLine("Welcome to Locked Door!");
+Console.WriteLine("Welcome to Door Master 2000!");
 Console.WriteLine("Please set a intial numeric passcode for the door");
 int IntialPin = Convert.ToInt32(Console.ReadLine());
 Door NewDoor = new Door(IntialPin);
 Console.WriteLine("Press any key to begin");
 Console.ReadKey();
+Console.Clear();
 
 
 
 
 while (true)
 {
-   
+    
+    string menu = UserMenu(NewDoor, IntialPin);
+    Console.ReadKey();
 
 
+}
 
+string UserMenu(Door newDoor, int intialPin)
+{
+    Console.WriteLine("**************************************************");
+    Console.WriteLine("* DOOR MASTER 2000                               *");
+    Console.WriteLine($"* Current Door Status: \t{newDoor.doorstatus}    ");
+    Console.WriteLine("**************************************************");
+    Console.WriteLine("* Please make a selection:                       *");
+    Console.WriteLine("* - (O)pen                                       *");
+    Console.WriteLine("* - (C)losed                                     *");
+    Console.WriteLine("* - (L)ock                                       *");
+    Console.WriteLine("* - (U)nlock                                     *");
+    Console.WriteLine("* - (S)et Pin                                    *");
+    Console.WriteLine("* - (E)xit                                       *");
+    Console.WriteLine("**************************************************");
+    return null;
 }
 
 public class Door
