8d7540362386b69074ca9884c0abe624d56a8e09
jb-network <85036487+jb-network@users.noreply.github.com>
2022-06-10 23:27:02 -0500

    Add project files.

---
+++ The_Locked_Door/Program.cs
@@ -0,0 +1,8 @@
+﻿// This is challenge work for the "C# Players Guide"
+// Level 24 "The Locked Door" Challenge
+//The door has four states Open, Closed, Locked, Unlocked
+// To unlock a a code must be used
+// The user must be able to change the code, if the current code is provided. 
+
+
+public enum DoorStatus { Open, Closed, Locked, Unlocked }
